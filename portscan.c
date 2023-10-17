cd5763adeeeb5193a713f7981829c01684462849
Leonardo <59527689+LeonardoTaddei04@users.noreply.github.com>
2023-10-17 14:36:49 +0200

    Add files via upload

---
+++ portscan.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Uso: %s <indirizzo IP>\n", argv[0]);
+        return 1;
+    }
+
+    char *target_ip = argv[1];
+    int port;
+
+    for (port = 1; port <= 1024; port++) {
+        int sock = socket(AF_INET, SOCK_STREAM, 0);
+        if (sock == -1) {
+            perror("socket");
+            return 1;
+        }
+
+        struct sockaddr_in target;
+        target.sin_family = AF_INET;
+        target.sin_port = htons(port);
+        target.sin_addr.s_addr = inet_addr(target_ip);
+
+        if (connect(sock, (struct sockaddr *)&target, sizeof(target)) == 0) {
+            printf("Porta %d aperta\n", port);
+        }
+
+        close(sock);
+    }
+
+    return 0;
+}
